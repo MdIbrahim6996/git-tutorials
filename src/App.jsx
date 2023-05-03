b74f498e778efdf27e18c3f4ac7cc6dd362d4596
Md. Ibrahim Hafeez <mdibrahimhaf6996@gmail.com>
2023-05-03 22:02:46 +0530

    Deleted Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import  './App.css'
 import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <div className='app'>
+      <main className='app'>
         App
-      </div>
-      <Footer/>
+      </main>
     </div>
   )
 }
