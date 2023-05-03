813bdc672552a6b5153cd069b4c114494e1f2eec
Md. Ibrahim Hafeez <mdibrahimhaf6996@gmail.com>
2023-05-03 21:52:09 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import  './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div>
-      App
+      <Navbar/>
+      <div className='app'>
+        App
+      </div>
+      <Footer/>
     </div>
   )
 }
